b9b9727e663bfef441fc89dc70b9d7a2b3c04732
V <chandra-muthu.v@capgemini.com>
2020-03-11 13:05:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit  } from '@angular/core';
+import * as $ from "jquery";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'temptwo';
+
+  ngAfterViewInit() {
+    console.log("---ngAfterViewInit() Demo---");
+
+    $(document).ready(function(){
+      $('.login-info-box').fadeOut();
+      $('.login-show').addClass('show-log-panel');
+  });
+  
+  
+  $('.login-reg-panel input[type="radio"]').on('change', function() {
+      if($('#log-login-show').is(':checked')) {
+          $('.register-info-box').fadeOut(); 
+          $('.login-info-box').fadeIn();
+          
+          $('.white-panel').addClass('right-log');
+          $('.register-show').addClass('show-log-panel');
+          $('.login-show').removeClass('show-log-panel');
+          
+      }
+      else if($('#log-reg-show').is(':checked')) {
+          $('.register-info-box').fadeIn();
+          $('.login-info-box').fadeOut();
+          
+          $('.white-panel').removeClass('right-log');
+          
+          $('.login-show').addClass('show-log-panel');
+          $('.register-show').removeClass('show-log-panel');
+      }
+  });
+
+
+  }
 }
+
+
